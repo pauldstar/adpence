314fec4cca3ac3c007a966159f3a8046c52a9316
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-10-16 01:56:37 +0100

    Update app.js
    init all popovers

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
+initPopover();
+
+/*
+* FUNCTIONS
+*/
+
 function wobble(el)
 {
     el.classList.add('wobble');
     setTimeout(_ => el.classList.remove('wobble'), 800);
+}
+
+function initPopover()
+{
+    let popoverTriggerList = [].slice.call(
+        document.querySelectorAll('[data-toggle="popover"]')
+    );
+
+    let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
 }
