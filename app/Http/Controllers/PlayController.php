8992ef5871cced7edb069b20edcbc779ab25fd82
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-10-17 04:47:41 +0100

    UPDATE move out "balance" properties and actions from play component

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -2,19 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-
 class PlayController extends Controller
 {
     public function __invoke()
     {
-        $balance = $this->getBalance();
-        return view('play', compact('balance'));
-    }
-
-    private function getBalance()
-    {
-        $balance = optional(Auth::user())->balance ?? session('balance') ?? 0;
-        return str_pad($balance, 4, 0, STR_PAD_LEFT);
+        return view('play');
     }
 }
--- app/Http/Livewire/Play.php
+++ app/Http/Livewire/Play.php
@@ -2,28 +2,13 @@
 
 namespace App\Http\Livewire;
 
-use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Play extends Component
 {
-    public int $balance;
-
-    public function mount()
-    {
-        $this->balance = optional(Auth::user())->balance ?? session('balance') ?? 0;
-    }
-
     public function play()
     {
-        $this->balance += 25;
-
-        if (Auth::guest()) session(['balance' => $this->balance]);
-        else Auth::user()->update(['balance' => $this->balance]);
-
-        $this->emit(
-            'balance-increment', str_pad($this->balance, 4, 0, STR_PAD_LEFT)
-        );
+        $this->emit('payment-received', 25);
     }
 
     public function render()
