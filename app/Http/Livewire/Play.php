ab86ce4e817728cb0be74622135f310969c02773
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-12-14 01:44:11 +0000

    Update Play.php
    run stopVideo() on video-stopped event
    emit video url on play-video event
    emit either increment-balance, or no-payment on video end

--- app/Http/Livewire/Play.php
+++ app/Http/Livewire/Play.php
@@ -2,13 +2,38 @@
 
 namespace App\Http\Livewire;
 
+use App\Video;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
 class Play extends Component
 {
+    protected array $listeners = [
+        'video-stopped' => 'stopVideo'
+    ];
+
     public function play()
     {
-        $this->emit('payment-received', 25);
+        $url = Video::inRandomOrder()->first()->url;
+
+        Session::put('videoStarted', time());
+
+        $this->emit(
+            'play-video',
+            $url . '?autoplay=1&amp;modestbranding=1&amp;showinfo=0'
+        );
+    }
+
+    public function stopVideo()
+    {
+        $start = Session::pull('videoStarted');
+        $elapsed = time() - $start;
+
+        if ($elapsed >= Video::$minPlayTime) {
+            $this->emit('increment-balance', Video::$paymentAmount);
+        } else {
+            $this->emit('no-payment');
+        }
     }
 
     public function render()
