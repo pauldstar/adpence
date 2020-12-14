ced4a56f2e296d2bf8c925f8e7ddf1abea1bde4c
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-12-14 01:48:20 +0000

    Update toast.js
    remove toast bg class after hidden

--- public/js/toast.js
+++ public/js/toast.js
@@ -3,20 +3,24 @@ const Toast = (_ => {
         _$toastText = document.getElementById('app-toast-text'),
         _bsToast = new bootstrap.Toast(_$toast);
 
-    _bsToast.addEventListener('hidden.bs.toast', _=> {
-
-    })
-
     function _show(msg, mode) {
-        _$toast.classList.add('bg' + mode);
+        _$toast.classList.add('bg-' + mode);
         _$toastText.innerHTML = msg;
+
         _bsToast.show();
+
+        let removeClass = _ => {
+            _$toast.classList.remove('bg-' + mode);
+            _$toast.removeEventListener('hidden.bs.toast', removeClass);
+        };
+
+        _$toast.addEventListener('hidden.bs.toast', removeClass);
     }
 
     return {
         success: msg => _show(msg, 'success'),
         error: msg => _show(msg, 'danger'),
         warning: msg => _show(msg, 'warning'),
-        info: msg => _show(msg, 'info'),
+        info: msg => _show(msg, 'info')
     };
 })();
